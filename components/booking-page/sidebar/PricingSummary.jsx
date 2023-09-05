012c8d3aa665fcf3ea0a60be9ff8a669d68672e2
PVDPrakash1 <69307993+PVDPrakash1@users.noreply.github.com>
2023-09-06 03:08:30 +0530

    chnahs

--- components/booking-page/sidebar/PricingSummary.jsx
+++ components/booking-page/sidebar/PricingSummary.jsx
@@ -1,14 +1,14 @@
-const PricingSummary = () => {
+const PricingSummary = ({tour}) => {
   return (
     <div className="px-30 py-30 border-light rounded-4 mt-30">
       <div className="text-20 fw-500 mb-20">Your price summary</div>
       <div className="row y-gap-5 justify-between">
         <div className="col-auto">
-          <div className="text-15">Superior Twin</div>
+          <div className="text-15">{tour.name}</div>
         </div>
         {/* End col */}
         <div className="col-auto">
-          <div className="text-15">US$3,372.34</div>
+          <div className="text-15">Rs.{tour.price}</div>
         </div>
         {/* End col */}
       </div>
@@ -19,7 +19,7 @@ const PricingSummary = () => {
           <div className="text-15">Taxes and fees</div>
         </div>
         <div className="col-auto">
-          <div className="text-15">US$674.47</div>
+          <div className="text-15">Rs.0</div>
         </div>
       </div>
       {/* End .row */}
@@ -34,7 +34,7 @@ const PricingSummary = () => {
       </div>
       {/* End .row */}
 
-      <div className="px-20 py-20 bg-blue-2 rounded-4 mt-20">
+      {/* <div className="px-20 py-20 bg-blue-2 rounded-4 mt-20">
         <div className="row y-gap-5 justify-between">
           <div className="col-auto">
             <div className="text-18 lh-13 fw-500">Price</div>
@@ -43,7 +43,7 @@ const PricingSummary = () => {
             <div className="text-18 lh-13 fw-500">US$4,046.81</div>
           </div>
         </div>
-      </div>
+      </div> */}
       {/* End .row */}
     </div>
     // End px-30
